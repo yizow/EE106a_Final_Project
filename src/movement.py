db3d0a338bf197c57ab32e05165597c9a35c1bde
Yizhe Liu <yizow@berkeley.edu>
2016-12-08 04:38:58 -0800

    Consistent Baxter movement
    Added new file movement.py. Contains a number of functions to control
    Baxter's arms.
    
    Changes "move" and "setup_motion" command in boba_bot to use these new
    functions.  Adds new command "reset" that brings one, or both arms of
    Baxter to a starting configuration, off-camera, with both grippers
    pointing straight forwards.
    
    Adds a new command "i". Whatever follows will be immediately executed by
    the python interpreter. This gives a useful way to debug code from
    within the scope of the MainLoop object.

---
+++ src/movement.py
@@ -0,0 +1,89 @@
+import sys
+
+import rospy
+
+import moveit_commander
+from moveit_msgs.msg import OrientationConstraint, Constraints
+from geometry_msgs.msg import PoseStamped
+
+def setup_motion():
+  moveit_commander.roscpp_initialize(sys.argv)
+  robot = moveit_commander.RobotCommander()
+  scene = moveit_commander.PlanningSceneInterface()
+  left_arm = moveit_commander.MoveGroupCommander('left_arm')
+  right_arm = moveit_commander.MoveGroupCommander('right_arm')
+  left_arm.set_planner_id('RRTConnectkConfigDefault')
+  left_arm.set_planning_time(5)
+  right_arm.set_planner_id('RRTConnectkConfigDefault')
+  right_arm.set_planning_time(5)
+  return left_arm, right_arm
+
+def create_goal(end_position):
+  goal = PoseStamped()
+  goal.header.frame_id = "base"
+
+  goal.pose.position.x = end_position[0]
+  goal.pose.position.y = end_position[1]
+  goal.pose.position.z = end_position[2]
+
+  set_goal_orientation(goal.pose)
+
+  return goal
+
+def set_goal_orientation(goal):
+  goal.orientation.x = 0.0
+
+
+  goal.orientation.y = 2**.5/2
+  goal.orientation.z = 0.0
+  goal.orientation.w = 2**.5/2
+
+def create_constraint(name):
+  orien_const = OrientationConstraint()
+  orien_const.link_name = name;
+  orien_const.header.frame_id = "base";
+  orien_const.orientation.y = 2**.5/2
+  orien_const.orientation.w = 2**.5/2
+  orien_const.absolute_x_axis_tolerance = 0.1;
+  orien_const.absolute_y_axis_tolerance = 0.1;
+  orien_const.absolute_z_axis_tolerance = 0.1;
+  orien_const.weight = 1.0;
+  consts = Constraints()
+  consts.orientation_constraints = [orien_const]
+  return consts
+
+def move_forward(arm, position):
+  steps = [(x, position.y, position.z) for x in range(position.x, 1., .05)]
+  # Make sure we end at a consistent location
+  if steps[-1][0] != .95:
+    steps.append((.95, position.y, position.z))
+
+  move_steps(arm, steps)
+
+def move_backward(position):
+  steps = [(x, position.y, position.z) for x in range(position.x, .2, -.05)]
+  # Make sure we end at a consistent location
+  if steps[-1][0] != .95:
+    steps.append((.95, position.y, position.z))
+
+  move_steps(arm, steps)
+
+def move_steps(arm, steps):
+  for step in steps:
+    move(arm, step)
+    rospy.sleep(.5)
+
+def move(arm, position):
+  goal = create_goal(position)
+  set_goal_orientation(goal.pose)
+
+  #Set the goal state to the pose you just defined
+  arm.set_pose_target(goal)
+
+  #Set the start state for the left arm
+  arm.set_start_state_to_current_state()
+
+  #Plan a path
+  plan = arm.plan()
+
+  arm.execute(plan)
